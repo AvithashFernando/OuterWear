3aae7946d66009abf9dd67b63c9df377e42c65b2
AvithashFernando <avithashfdo99@gmail.com>
2024-04-03 13:21:53 +0530

    Initial Commit

---
+++ OuterWear/OuterWearApp.swift
@@ -0,0 +1,17 @@
+//
+//  OuterWearApp.swift
+//  OuterWear
+//
+//  Created by NIBM-LAB04-PC07 on 2024-04-03.
+//
+
+import SwiftUI
+
+@main
+struct OuterWearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
